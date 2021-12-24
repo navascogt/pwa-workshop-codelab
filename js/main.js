bc7358b16da5ae080c04121fd43a40fa7ea24947
Douglas Navas <douglas.l@navascogt.com>
2021-12-23 18:45:15 -0600

    Cambio de maquina

--- js/main.js
+++ js/main.js
@@ -28,3 +28,21 @@ window.addEventListener('DOMContentLoaded', async () => {
 
   editor.setContent(defaultText);
 });
+
+// import swURL from 'sw:../service-worker.js';
+import swURL from './service-worker.js';
+xconst swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  // Wait for the 'load' event to not block other work
+  window.addEventListener('load', async () => {
+    // Try to register the service worker.
+    try {
+      const reg = await navigator.serviceWorker.register(swURL);
+      console.log('Service worker registered! 😎', reg);
+    } catch (err) {
+      console.log('😥 Service worker registration failed: ', err);
+    }
+  });
+}
